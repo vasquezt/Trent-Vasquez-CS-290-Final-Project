c14d58f6ec7829a858fc18b4535f823ab1904d82
Trent Vasquez <vasquezt@oregonstate.edu>
2017-06-06 16:24:07 -0700

    Added base files for articles, started Css

---
+++ public/articleTemplate.js
@@ -0,0 +1,14 @@
+(function() {
+  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
+templates['article'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
+
+  return "<article class=\"full-article\">\n	<main class=\"text-box\">\n		<p class=\"article-title\">\n			"
+    + alias4(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"title","hash":{},"data":data}) : helper)))
+    + "\n		</p>\n	\n		<p class=\"article-text\">\n			"
+    + alias4(((helper = (helper = helpers.text || (depth0 != null ? depth0.text : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"text","hash":{},"data":data}) : helper)))
+    + "\n		</p>\n	</main>\n	<i class=\"article-img\">\n		"
+    + alias4(((helper = (helper = helpers.image || (depth0 != null ? depth0.image : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"image","hash":{},"data":data}) : helper)))
+    + "\n	</i>\n</article>\n";
+},"useData":true});
+})();
--- server.js
+++ server.js
@@ -8,6 +8,9 @@ var express = require('express');
 var handlebars = require('handlebars');
 var exphbs = require('express-handlebars');
 
+var secureArticleData = require('./secureArticleData');
+var articleData = require('./articleData');
+
 var port = process.env.PORT || 9001;
 console.log('Server is running on port: ' + port);
 
@@ -18,7 +21,23 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', '.handlebars');
 
 app.get('/', function(req, res, next){
-	res.render('homepage');
+	if(secureArticleData){
+		if(articleData){
+			var templateArgs = {
+				articleDataNorm: articleData,
+				articleDataCur: secureArticleData
+			}
+		}else{
+			var templateArgs = {
+				articleDataCur: secureArticleData
+			}
+		}
+		res.render('homepage', templateArgs);
+	}else{
+		res.render('404Page');
+	}	
+
+	
 });
 
 app.get('*', function(req, res){
