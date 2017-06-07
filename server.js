ce7efc431188a616c22d431ad9ddd0c3d25fb3ef
Trent Vasquez <vasquezt@oregonstate.edu>
2017-06-06 23:14:00 -0700

    Brain is Spagheti, choose spec page, start add art

--- server.js
+++ server.js
@@ -40,6 +40,54 @@ app.get('/', function(req, res, next){
 	
 });
 
+app.get('/:index', function(req, res, next){
+	var page = req.params.index;
+	var pageFound = 0;
+
+	if(page == 'newArticle'){
+		var templateArgs = {
+		}
+		res.render('newArticle', templateArgs);
+		pageFound = 1;
+	}
+
+	if(secureArticleData){
+		var numArticles = secureArticleData.length;
+		for(i = 0; i < numArticles; i++){
+			if(secureArticleData[i].title == page){
+				subArray = [];
+				subArray[0] = secureArticleData[i]
+				templateArgs = {
+					articleDataCur: subArray
+				}
+				pageFound = 1;
+				res.render('homepage', templateArgs);
+				break;
+			}
+		}
+	}
+	if(pageFound == 0){
+		if(articleData){
+			var numArticles = articleData.length;
+			for(i = 0; i < numArticles; i++){
+				if(articleData[i].title == page){
+					subArray = [];
+					subArray[0] = articleData[i]
+					templateArgs = {
+						articleDataCur: subArray
+					}
+					pageFound = 1;
+					res.render('homepage', templateArgs);
+					break;
+				}
+			}
+		}
+	}
+	if(pageFound == 0){
+		res.render('404Page');
+	}
+});
+
 app.get('*', function(req, res){
 	res.status(404);
 	res.render('404Page');
